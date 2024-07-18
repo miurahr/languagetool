0b86ad09eda2e21dbcb9555de17be5dfe12c2dce
Hiroshi Miura <miurahr@linux.com>
2024-07-18 09:50:35 +0900

    chore: Bump lucene@8.11.3 and gosen@8.11.0
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- languagetool-language-modules/ja/build.gradle.kts
+++ languagetool-language-modules/ja/build.gradle.kts
@@ -4,16 +4,7 @@ plugins {
 
 dependencies {
     implementation(libs.jetbrains.annotations)
-    implementation("com.github.lucene-gosen:lucene-gosen:6.2.1:ipadic") {
-        exclude(group = "com.ibm.icu", module = "icu4j")
-        exclude(group = "org.apache.lucene", module = "lucene-analyzers")
-        exclude(group = "org.apache.lucene", module = "lucene-analyzers-common")
-        exclude(group = "org.apache.lucene", module = "lucene-codecs")
-        exclude(group = "org.apache.lucene", module = "lucene-core")
-        exclude(group = "org.apache.solr", module = "solr-core")
-        exclude(group = "org.apache.solr", module = "solr-solrj")
-    }
-
+    implementation("org.omegat.lucene:lucene-gosen:8.11.0:ipadic")
     implementation(libs.icu4j)
     implementation(project(":languagetool-core"))
     testImplementation(libs.junit4)
