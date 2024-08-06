ece16edd18ea88ce6e93bbb8dd08f61a1c9a903e
Hiroshi Miura <miurahr@linux.com>
2024-08-06 16:23:46 +0900

    chore: update languagetool-dev and -wikipedia
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- languagetool-standalone/build.gradle.kts
+++ languagetool-standalone/build.gradle.kts
@@ -10,13 +10,23 @@ dependencies {
     implementation(project(":languagetool-tools"))
     implementation(libs.jetbrains.annotations)
     implementation(libs.commons.collections4)
+    implementation(libs.commons.io)
+    implementation(libs.commons.lang)
     implementation(libs.lucene.core)
     implementation(project(":language-en"))
     runtimeOnly(project(":language-all"))
     testImplementation(libs.junit4)
     testImplementation(libs.mockito.core)
+    testImplementation(libs.language.detector)
+    testImplementation(project(":language-all"))
     testImplementation(testFixtures(project(":languagetool-core")))
     testImplementation(libs.logback.classic)
 }
 
 description = "LanguageTool stand-alone GUI"
+
+tasks.withType<Test> {
+    minHeapSize = "512m"
+    maxHeapSize = "2048m"
+    jvmArgs = listOf("-XX:MaxMetaspaceSize=512m")
+}
